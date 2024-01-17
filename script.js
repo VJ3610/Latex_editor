ec7de54e1f5f9c011bcac89812c752f214f9accf
Varun Raj <varunraj0609@gmail.com>
2024-01-17 19:48:54 +0530

    js

--- script.js
+++ script.js
@@ -1,13 +1,44 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-  const codeInput = document.getElementById('codeInput');
-  const latexOutput = document.getElementById('latexOutput');
-  const codeOutput = document.getElementById('codeOutput');
+// document.addEventListener('DOMContentLoaded', function () {
+//   const script = document.createElement('script');
+//   script.src = 'https://cdnjs.cloudflare.com/ajax/libs/KaTeX/0.12.0/katex.min.js';
+//   script.integrity = 'sha512-+lT03oxUO9nbR1+2A4UB9iPhwMG/NSNmrzUzvcvzoQK8n3gm/X2ClNq+bu6i6GkmS+te++TOebda7CEdRguCgA==';
+//   script.crossOrigin = 'anonymous';
 
-  codeInput.addEventListener('input', function() {
-    // Display LaTeX symbols in the latex_eq div
-    latexOutput.innerHTML = codeInput.value;
-  });
+//   script.onload = function () {
+//     window.updateLatex = function (latexCode) {
+//       const latexCodeOutput = document.getElementById('latexOutput');
+//       const latexDisplayOutput = document.getElementById('codeOutput');
 
-  // You can integrate a code execution mechanism here and display the output in the latex_out div
-});
+//       katex.render(latexCode, latexCodeOutput, { throwOnError: false });
+//       latexDisplayOutput.textContent = latexCode;
+//     };
+//   };
+
+//   document.head.appendChild(script);
+
+//   // Function to handle button click
+//   function handleButtonClick(latexCode) {
+//     const latexCodeOutput = document.getElementById('latexOutput');
+//     const latexDisplayOutput = document.getElementById('codeOutput');
+
+//     // Render LaTeX code in the latexOutput div
+//     katex.render(latexCode, latexCodeOutput, { throwOnError: false });
+
+//     // Display the entered LaTeX code in the codeOutput div
+//     latexDisplayOutput.textContent = latexCode;
+//   }
+
+//   // Get all buttons and add click event listeners
+//   const buttons = document.querySelectorAll('.latex_eq button');
+//   buttons.forEach(function (button) {
+//     button.addEventListener('click', function () {
+//       const latexCode = button.getAttribute('data-latex');
+//       handleButtonClick(latexCode);
+//     });
+//   });
+// });
+
+function updateLatex(latex) {
+  var currentLatex = document.getElementById('latexOutput').value;
+  document.getElementById('latexOutput').value = currentLatex + latex;
+ }
